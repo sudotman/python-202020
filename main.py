bbb2f162afa53c4fbfa81e4a777922b1868bccd7
Satyam Kashyap <satyamsudo@gmail.com>
2021-12-01 01:37:56 +0530

    Fully working notification at periodic time periods :)

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from PyQt5.QtQml import QQmlApplicationEngine
 from PyQt5.QtCore import QObject, pyqtSignal
 import threading
 from time import sleep
+import time
+from win10toast import ToastNotifier
 
 # Creating QObject subclass to be able to utilize python threads to send signals to our qt frontend
 class Backend(QObject):
@@ -17,6 +19,8 @@ class Backend(QObject):
         self.updated.emit(curr_time)
       
     def bootUp(self):
+        timeInitial = time.time()
+
         t_thread = threading.Thread(target=self._bootUp)
         t_thread.daemon = True
         t_thread.start()
@@ -26,6 +30,18 @@ class Backend(QObject):
             curr_time = strftime("%H:%M:%S", localtime())
             self.updater(curr_time)
             sleep(0.1)
+
+    def do2020(self):
+        t1_thread = threading.Thread(target=self._do2020)
+        t1_thread.daemon = True
+        t1_thread.start()
+
+    def _do2020(self):
+        toast = ToastNotifier()
+        while True:
+            toast.show_toast("Hey there!","Look away at something 20 metres away for 20 seconds. Thanks! And good luck with your work :)",icon_path='./icon.ico',duration=25)
+            print("Displayed toast")
+            sleep(7)
 
 #Time stuff
 curr_time = strftime("%H:%M:%S", localtime())
@@ -39,6 +55,7 @@ engine.load('./UI/main.qml')
 back_end = Backend()
 engine.rootObjects()[0].setProperty('backend', back_end)
 back_end.bootUp()
+back_end.do2020()
 #engine.rootObjects()[0].setProperty('currTime', curr_time) # Sending our current time variable over to our UI
 
 sys.exit(app.exec())
